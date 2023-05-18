5761423a5726e570e3eed4c2d5205a32b9bd57ad
jhonyrozwod <jhonathanrozwod@gmail.com>
2023-05-18 20:04:47 -0300

    UHUL

--- config/db.js
+++ config/db.js
@@ -1,5 +1,6 @@
+
 if(process.env.NODE_ENV == "production"){
-    module.exports = {mongoURI: "mongodb+srv://jhonathanrozwod:<1BXcZRjOKpcCWCnX>@cluster0.bntsybc.mongodb.net/"}
+    module.exports = {mongoURI: "mongodb+srv://jhonathanrozwod:1BXcZRjOKpcCWCnX@cluster0.bntsybc.mongodb.net/"}
 }else{
 module.exports = {mongoURI: "mongodb://127.0.0.1:27017/blogapp'"}
 
